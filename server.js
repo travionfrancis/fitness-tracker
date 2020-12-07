870d46789d7d019ae7354982dbbdc4d854745ddd
TraviFravi <travi1500@gmail.com>
2020-12-06 22:20:09 -0500

    HEROKU PLEASE WORKhttps://github.com/travionfrancis/tracker-of-fitness

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
-// Importing express server module
 const express = require("express");
 const path = require("path");
-// Importing the mongoose and morgan logger modules
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Setting the Express App
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8008;
 
-// Setting up logger
 app.use(logger("dev"));
 
-// Setting the Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Connecting to the db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/columbia", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 });
 
-// Routes
-// For static files, look in the public folder
 app.use(express.static(path.join(__dirname, 'public')));
-// API Routes
+
 app.use("/api", require("./routes/apiRoutes.js"));
-// HTML Routes
+
 app.use("/", require("./routes/htmlRoutes.js"));
 
-// Starting the server
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
