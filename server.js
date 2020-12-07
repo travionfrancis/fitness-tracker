17b7a3fd1ee2b59dba7e7528352968641c41822f
TraviFravi <travi1500@gmail.com>
2020-12-06 20:29:43 -0500

    fixed routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
+
+const htmlRouter = require("./routes/view.js");
+const apiRouter = require("./routes/api.js");
 
 const app = express();
 
@@ -14,18 +16,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Columbia',
+  process.env.MONGODB_URI || 'mongodb://localhost/columbia',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use("/", htmlRouter);
+app.use(apiRouter);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
