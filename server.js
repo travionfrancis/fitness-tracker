d5dc24972b9cfcdaf37ebbfd8f68a55bc2557a3b
TraviFravi <travi1500@gmail.com>
2020-12-06 20:01:47 -0500

    Fixing up the Heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/Columbia',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
